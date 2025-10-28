fd859302ad709ac9ab698c4bd0a9b4d8e8f41031
chu0802 <yuchuyu@cs.ubc.ca>
2025-10-28 08:49:49 -0700

    <update>: add a trainer to handling training progress

--- src/train/trainer.py
+++ src/train/trainer.py
@@ -1,7 +1,7 @@
 from accelerate import Accelerator
 from src.utils import Arg, argument_parser
 from pathlib import Path
-from src.lookwhere.modeling import Selector
+from src.lookwhere.modeling import load_model
 from src.lookwhere.transforms import trans
 from src.utils import seed_everything, none_or_str
 from torch.utils.data import Dataset, DataLoader
@@ -15,12 +15,13 @@ import wandb
 
 
 class MaskedDataset(Dataset):
-    def __init__(self, input_dataset, groundtruth_dataset, input_masked_ratio=[0.1], output_masked_ratio=0.0):
+    def __init__(self, input_dataset, groundtruth_dataset, input_masked_ratio=[0.1], output_masked_ratio=0.0, low_res_size=154):
         super().__init__()
         self.input = input_dataset
         self.groundtruth = groundtruth_dataset
         self.input_masked_ratio = input_masked_ratio
         self.output_masked_ratio = output_masked_ratio
+        self.low_res_size = low_res_size
 
     def __len__(self):
         return len(self.input)
@@ -32,13 +33,84 @@ class MaskedDataset(Dataset):
         ratio = self.input_masked_ratio[torch.randint(len(self.input_masked_ratio), (1,))]
         
         # randomly mask the input image
-        mask = (torch.rand(1, image.shape[1], image.shape[2]) >= ratio).to(image.dtype)
+        mask = (torch.rand(1, self.low_res_size, self.low_res_size) >= ratio).to(image.dtype)
+            
+        return image, mask, selector_map
+
+class Trainer:
+    def __init__(self, accelerator, model, dataloader, num_epoches=10, output_dir=Path("output/models"), device="cuda"):
+        self.accelerator = accelerator
+        self.model = model
+        self.dataloader = dataloader
+        self.device = device
+        self.output_dir = output_dir
+        self.output_dir = output_dir
+
+        self.num_epoches = num_epoches
         
-        masked_input = image * mask
+        self.optimizer, self.scheduler = self.init_optimizer_scheduler()
+
+        self.model, self.dataloader, self.optimizer, self.scheduler = self.accelerator.prepare(
+            self.model, self.dataloader, self.optimizer, self.scheduler
+        )
         
-        return masked_input, selector_map
+        self.criterion = nn.KLDivLoss(reduction="batchmean", log_target=True)
+        self.global_steps = 0
+
+    def init_optimizer_scheduler(self):
+        optimizer = optim.AdamW(
+            self.model.parameters(),
+            lr=1e-4,
+            weight_decay=0.05,
+            betas=(0.9, 0.999),
+            eps=1e-8,
+        )
         
+        scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=self.num_epoches * len(self.dataloader), eta_min=1e-6)
+        return optimizer, scheduler
+
+    def train(self):
+        disable_bar = not self.accelerator.is_main_process
         
+        for epoch in tqdm(range(self.num_epoches), desc="Epochs", disable=disable_bar):
+            batch_bar = tqdm(
+                self.dataloader,
+                desc=f"Epoch {epoch+1}/{self.num_epoches}",
+                total=len(self.dataloader),
+                leave=False,
+                disable=disable_bar,
+            )
+
+            for batch in batch_bar:
+                self.optimizer.zero_grad()
+                inputs, masks, targets = batch
+                outputs = self.model(inputs, masks)
+                loss = self.criterion(F.log_softmax(outputs["selector_map"], dim=1), F.log_softmax(targets, dim=1))
+                self.accelerator.backward(loss)
+                self.optimizer.step()
+                self.scheduler.step()
+
+                if self.accelerator.is_main_process:
+                    lr_now = self.scheduler.get_last_lr()[0]
+                    wandb.log(
+                        {
+                            "train/loss": loss.item(),
+                            "train/lr": lr_now,
+                        },
+                        step=self.global_steps,
+                    )
+                self.global_steps += 1
+
+        self.finish()
+    
+    def finish(self):
+        if self.accelerator.is_main_process:
+            self.output_dir.mkdir(parents=True, exist_ok=True)
+            unwrapped = self.accelerator.unwrap_model(self.model)
+            torch.save(unwrapped.state_dict(), self.output_dir / "selector_final.pt")
+
+            wandb.finish()
+
 
 def main(args):
     seed_everything(args.seed)
@@ -46,17 +118,10 @@ def main(args):
     accelerator = Accelerator()
     device = accelerator.device
     
-    if args.pretrained_params_path is not None:
-        pretrained_params = torch.load(args.pretrained_params_path, map_location="cpu", weights_only=True)["selector"]
-    else:
-        pretrained_params = None
+    lw = load_model(pretrained_params_path=args.pretrained_params_path, head_params_path=args.head_params_path, device="cpu")
 
-    selector = Selector(
-        pretrained_params=pretrained_params,
-        lw_type="dinov2",
-        hr_size=518,
-        device=device,
-    )
+    selector = lw.selector
+    selector.to(device)
     
     if accelerator.is_main_process:
         wandb.init(
@@ -91,74 +156,30 @@ def main(args):
         shuffle=True,
         drop_last=True,
         num_workers=4,
-        pin_memory=True,
     )
     
-    optimizer = optim.AdamW(
-        selector.parameters(),
-        lr=1e-4,
-        weight_decay=0.05,
-        betas=(0.9, 0.999),
-        eps=1e-8,
+    exp_name = f"seed_{args.seed}_mask_{'_'.join([str(r) for r in args.input_masked_ratio])}_{args.output_masked_ratio}_epoches_{args.num_epoches}"
+    
+    trainer = Trainer(
+        accelerator=accelerator,
+        model=selector,
+        dataloader=train_dataloader,
+        num_epoches=args.num_epoches,
+        output_dir=args.final_output_dir / exp_name,
+        device=device,
     )
     
-    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.num_epoches * len(train_dataloader), eta_min=1e-6)
+    trainer.train()
 
-    selector, train_dataloader, optimizer, scheduler = accelerator.prepare(
-        selector, train_dataloader, optimizer, scheduler
-    )
-    
-    criterion = nn.KLDivLoss(reduction="batchmean", log_target=True)
-    global_step = 0
-    selector.train()
-
-    disable_bar = not accelerator.is_main_process
-    
-    for epoch in tqdm(range(args.num_epoches), desc="Epochs", disable=disable_bar):
-        batch_bar = tqdm(
-            train_dataloader,
-            desc=f"Epoch {epoch+1}/{args.num_epoches}",
-            total=len(train_dataloader),
-            leave=False,
-            disable=disable_bar,
-        )
-
-        for batch in batch_bar:
-            optimizer.zero_grad()
-            inputs, targets = batch
-            outputs = selector(inputs)
-            loss = criterion(F.log_softmax(outputs["selector_map"], dim=1), F.log_softmax(targets, dim=1))
-            accelerator.backward(loss)
-            optimizer.step()
-            scheduler.step()
-            
-            if accelerator.is_main_process:
-                lr_now = scheduler.get_last_lr()[0]
-                wandb.log(
-                    {
-                        "train/loss": loss.item(),
-                        "train/lr": lr_now,
-                    },
-                    step=global_step,
-                )
-            global_step += 1
-
-    if accelerator.is_main_process:
-        exp_name = f"seed_{args.seed}_mask_{'_'.join([str(r) for r in args.input_masked_ratio])}_{args.output_masked_ratio}_epoches_{args.num_epoches}"
-        save_dir = args.final_output_dir / exp_name
-        save_dir.mkdir(parents=True, exist_ok=True)
-        unwrapped = accelerator.unwrap_model(selector)
-        torch.save(unwrapped.state_dict(), save_dir / "selector_final.pth")
-
-        wandb.finish()
 
 if __name__ == "__main__":
     args = argument_parser(
         Arg("-d", "--dataset_dir", type=Path, default=Path("/home/yuchuyu/project/lookwhere/output/subsample_maps/128000")),
         Arg("-s", "--seed", type=int, default=1102),
         Arg("-p", "--pretrained_params_path", type=none_or_str, default="models/lookwhere_dinov2.pt"),
+        Arg("-e", "--head_params_path", type=none_or_str, default="models/imagenet_classifier_head_9.pt"),
         Arg("-n", "--num_epoches", type=int, default=10),
-        Arg("-i", "--input_masked_ratio", type=float, default=[0.2], nargs="+"),
+        Arg("-i", "--input_masked_ratio", type=float, default=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9], nargs="+"),
         Arg("-o", "--output_masked_ratio", type=float, default=0.0),
         Arg("-f", "--final_output_dir", type=Path, default=Path("/home/yuchuyu/project/lookwhere/models")),
     )
